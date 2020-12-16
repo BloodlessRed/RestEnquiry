4cc77dabebfd95bff65dc6eb213d2fde569627cb
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2020-12-16 16:37:11 +0400

    Switched to postgres database

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 public class Car {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private int id;
 
     @Size(min = 1, max = 16)
